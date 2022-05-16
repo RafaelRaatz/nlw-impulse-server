b0bbc486ad39ef8a89ea4eefbdb97640071ce986
Rafael Raatz <mrrafa.raatz@gmail.com>
2022-05-16 18:09:48 -0300

    Add routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,8 @@ export const routes = express.Router()
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot} = req.body;
 
+    try{
+
         const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
         const nodemailerMailAdapter = new NodemailerMailAdapter()
 
@@ -23,4 +25,11 @@ routes.post('/feedbacks', async (req, res) => {
     })
     
     return res.status(201).send()
+} catch (err) {
+    console.log(err)
+
+    return res.status(500).send();
+}
+
+
 })
